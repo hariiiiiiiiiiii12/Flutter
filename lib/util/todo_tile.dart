29c209fc3f9ff850777b3c3bbeedcc48af19098e
hariiiiiiiiiiii12 <harimsdian07@gmail.com>
2026-05-27 19:21:34 +0530

    Initial Commit - ToDo

---
+++ lib/util/todo_tile.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ToDoTile extends StatelessWidget {
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+  ToDoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        child: Row(
+          children: [
+            Text(
+              taskName,
+              style: TextStyle(
+                decoration: taskCompleted
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
+            ),
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+            ),
+          ],
+        ),
+        padding: EdgeInsets.all(25),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
+  }
+}
